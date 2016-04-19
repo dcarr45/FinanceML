382494af9661c06653a033de85c21a9643b610b7
Danny Carr <dpcarr45@gmail.com>
2016-04-19 05:33:27 -0500

    readded 13F

---
+++ 13F/README.md
@@ -0,0 +1,1 @@
+# FinanceML
---
+++ 13F/cusip.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+if __name__ == "__main__":
+
+    out = open('cusips.txt', 'w')
+    for line in list(open('cusip.csv')):
+        cols = line.split(",")
+        out.write(cols[len(cols) - 1])
+
