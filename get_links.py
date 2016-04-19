22ec711964ccd3330406a717aeccb958989c0c28
Danny Carr <dpcarr45@gmail.com>
2016-04-19 05:07:39 -0500

    Initial commit

---
+++ get_links.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+from subprocess import call
+import sys
+import os
+import re
+
+
+if __name__ == "__main__":
+
+	for dir in os.listdir('.'):
+		if not os.path.isfile(dir):
+			
+			#receives file from stdin
+			links = []
+			unique_ids = set()
+
+			#since the 13F-HR/A (amendment forms) are last in the file,
+			#reverse to add most recent files first and no duplicates
+			file = open(dir + '/index.txt')
+			
+			for line in reversed(list(file)):
+				cols = re.split("\s{2,}", line) #two or more spaces
+
+				cik, form, link = cols[2], cols[0], cols[4]
+
+				#check for dups
+				if cik not in unique_ids:
+					links.append(link)
+					unique_ids.add(cik)
+
+			out = open(dir + '/links.txt', 'w')
+			
+			for link in links:
+				path = 'ftp://ftp.sec.gov/' + link
+				dest = dir + '/forms'
+				out.write(path + '\n')
+				
+
+
