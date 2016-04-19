147d2c46558829dc94b14d082574074809a87ca4
Danny Carr <dpcarr45@gmail.com>
2016-04-19 05:16:48 -0500

    actual indent fix

--- get_links.py
+++ get_links.py
@@ -8,33 +8,32 @@ import re
 
 if __name__ == "__main__":
 
-    for dir in os.listdir('.'):
-		if not os.path.isfile(dir):
-			
-			#receives file from stdin
-			links = []
-			unique_ids = set()
+for dir in os.listdir('.'):
+    if not os.path.isfile(dir):
 
-			#since the 13F-HR/A (amendment forms) are last in the file,
-			#reverse to add most recent files first and no duplicates
-			file = open(dir + '/index.txt')
-			
-			for line in reversed(list(file)):
-				cols = re.split("\s{2,}", line) #two or more spaces
+        #receives file from stdin
+        links = []
+        unique_ids = set()
 
-				cik, form, link = cols[2], cols[0], cols[4]
+        #since the 13F-HR/A (amendment forms) are last in the file,
+        #reverse to add most recent files first and no duplicates
+        file = open(dir + '/index.txt')
 
-				#check for dups
-				if cik not in unique_ids:
-					links.append(link)
-					unique_ids.add(cik)
+        for line in reversed(list(file)):
+            cols = re.split("\s{2,}", line) #two or more spaces
 
-			out = open(dir + '/links.txt', 'w')
-			
-			for link in links:
-				path = 'ftp://ftp.sec.gov/' + link
-				dest = dir + '/forms'
-				out.write(path + '\n')
-				
+            cik, form, link = cols[2], cols[0], cols[4]
+
+            #check for dups
+            if cik not in unique_ids:
+                links.append(link)
+                unique_ids.add(cik)
+
+        out = open(dir + '/links.txt', 'w')
+
+        for link in links:
+            path = 'ftp://ftp.sec.gov/' + link
+            dest = dir + '/forms'
+            out.write(path + '\n')
 
 
