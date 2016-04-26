ab11e28e36e37494e907ecd0faeba8cc71a48247
Danny Carr <dpcarr45@gmail.com>
2016-04-26 15:35:07 -0500

    fixed extract

--- 13F/extract.py
+++ 13F/extract.py
@@ -1,34 +1,37 @@
 #!/usr/bin/env python
 
-from pyspark import SparkConf, SparkContext
-
+from subprocess import call
 import sys
 import os
-from bs4 import BeautifulSoup
+import re
 
-def extract(spark):
-    cusips = set()
-    for line in list(open('cusip.txt')):
-        cusips.add(line.replace('"','').strip())
-    
-    forms = spark.textFile('hdfs:///user/carrdp/FinanceML/13F/all_forms/test_forms') \
-        .map(lambda form: getInfoTable(form, cusips)) \
-        .saveAsTextFile('hdfs:///user/carrdp/FinanceML/13F/res')
-
-def getInfoTable(text, cusips):
-    #receives form as indented xml
-    soup = BeautifulSoup(text)
-
-    for tag in soup.find_all('infoTable'):
-        if tag.cusip.string in cusips
-        
-    
-         
 
 if __name__ == "__main__":
-    conf = SparkConf()
-    conf.setMaster("spark://10.0.22.241:7077")
-    conf.set("spark.executor.memory", "10g")
-    conf.set("spark.driver.memory", "10g")
-    spark = SparkContext(conf = conf)
-    extract(spark)
+
+    for dir in os.listdir('.'):
+        if not os.path.isfile(dir):
+            
+            #receives file from stdin
+            links = []
+            unique_ids = set()
+
+            #since the 13F-HR/A (amendment forms) are last in the file,
+            #reverse to add most recent files first and no duplicates
+            file = open(dir + '/index.txt')
+            
+            for line in reversed(list(file)):
+                cols = re.split("\s{2,}", line) #two or more spaces
+
+                cik, form, link = cols[2], cols[0], cols[4]
+
+                #check for dups
+                if cik not in unique_ids:
+                    links.append(link)
+                    unique_ids.add(cik)
+
+            out = open(dir + '/links.txt', 'w')
+            
+            for link in links:
+                path = 'ftp://ftp.sec.gov/' + link
+                dest = dir + '/forms'
+                out.write(path + '\n')
