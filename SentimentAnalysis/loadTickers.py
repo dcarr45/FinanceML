b314395b839f9484cc4f08a982cb10e01d910ec3
Ellis Brown <elbrown47@gmail.com>
2016-04-28 21:40:14 -0500

    outsourced company list to csv

--- SentimentAnalysis/loadTickers.py
+++ SentimentAnalysis/loadTickers.py
@@ -5,8 +5,7 @@ import requests
 import webbrowser
 
 def loadTickers():
-    filename = 'tickers.csv'
-    f = open(filename, 'r')
+    f = open('tickers.csv', 'r')
     lines = f.readlines()
     f.close()
     tickers = [line.strip().replace('^','').replace('-','.') for line in lines[1:]]
@@ -17,22 +16,20 @@ def get_content(url):
     response = requests.get(url)
     return response.content
 
-def get_companies():
+def get_companies_from_tickers():
     mw = 'http://www.marketwatch.com/investing/stock/{ticker}'
     urls = [mw.format(ticker=t) for t in tickers]
-    #webbrowser.open_new_tab(urls[0])
-    #get_content(urls[0])
-    # c = get_content(urls[0])
-    # f = open('mwtest.html','w')
-    # f.write(c)
-    # f.close()
-    ret = []
+
+    f = open('companies.csv','w')
+    writer = csv.writer(f)
+
     for url in urls:
         content = get_content(url)
-        ret.append(content.split('<meta property="og:title" content="')[1].split('"')[0])
-    return ret
+        writer.writerows(content.split('<meta property="og:title" content="')[1].split('"')[0])
+    f.close()
 
-def loadCompanies()
+def loadCompanies():
+    return 0
 
 #(year, month, day)
 start = datetime.datetime(2000, 1, 1)
@@ -41,7 +38,5 @@ end = datetime.datetime(2016, 3, 20)
 tickers = loadTickers()
 companies = loadCompanies()
 
-def maxwrite():
-    f = open('vol_' + ticker + '.csv', 'w')
-    writer = csv.writer(f)
-    writer.writerows(vol)
+if __name__ == '__main__':
+    get_companies_from_tickers()
