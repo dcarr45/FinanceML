3f0c70768bebb028032959dc06ea1b47b01d3b6a
maxpudi <mwpuidak@gmail.com>
2016-04-28 20:30:10 -0500

    rough Data Retreival

--- technical/loadTickers.py
+++ technical/loadTickers.py
@@ -1,5 +1,15 @@
 #loadTickers.py
 
+import pandas_datareader.data as web
+import datetime
+import numpy as np
+from pprint import pprint
+import matplotlib.pyplot as plt
+import csv
+
+#(year, month, day)
+start = datetime.datetime(2000, 1, 1)
+end = datetime.datetime(2016, 3, 20)
 
 def loadTickers():
     filename = 'tickers.csv'
@@ -7,12 +17,10 @@ def loadTickers():
     lines = f.readlines()
     f.close()
     tickers = []
-
     for line in lines[1:]:
         line = line.strip()
         tickers.append(line)
-
-
     print tickers
+    return tickers
 
 loadTickers()
--- technical/transformData.py
+++ technical/transformData.py
@@ -10,6 +10,9 @@ import csv
 start = datetime.datetime(2000, 1, 1)
 end = datetime.datetime(2016, 3, 20)
 
+#trading days ahead to predict
+time_horizon = 30
+
 def loadTickers():
     filename = 'tickers.csv'
     f = open(filename, 'r')
@@ -19,16 +22,13 @@ def loadTickers():
     for line in lines[1:]:
         line = line.strip()
         tickers.append(line)
+    print tickers
     return tickers
 
 def loadRawData(tickers):
     for ticker in tickers:
         df = web.DataReader(ticker, 'yahoo', start, end)
         df.to_csv(ticker+ ".csv", sep = ',')
-
-
-#trading days ahead to predict
-time_horizon = 30
 
 def getPrices(ticker):
     # csv format
@@ -49,7 +49,7 @@ def getPrices(ticker):
 # # adjusted close: time series of array
 def volCalc(ticker, prices, lagTime):
         if '^' in ticker:
-            vol = prices
+            vol = prices[30:]
             f = open(ticker + '.csv', 'wb')
             # writer = csv.writer(f)
             # writer.writerows(prices)
@@ -64,7 +64,7 @@ def volCalc(ticker, prices, lagTime):
                 line = [prices[i][0], prices[i][1], sigma30]
                 vol.append(line)
                 delta = 0
-            f = open('vol' + ticker + '.csv', 'wb')
+            f = open('vol_' + ticker + '.csv', 'wb')
         writer = csv.writer(f)
         writer.writerows(vol)
 
@@ -74,45 +74,44 @@ def percentChange(p0,p1):
     return abs(p1-p0)/p0
 
 
-def RSI(ticker, prices, lagTime):
-    RSI_time = 14
-    # for i in range(lagTime,len(prices)):
-    for i in range(lagTime,lagTime +4):
-        RS = 0
+def RSICalc(ticker,prices, n=14):
+    date = zip(*prices)[0]
+    p = zip(*prices)[1]
+    price = [float(i) for i in p]
 
-        for day in range(0, RSI_time):
-            delta.append(float(prices[i-day][1]) - float(prices[i-day-1][1]))
-            dDown= [d for d in delta if d < 0]
-            dUp= [d for d in delta if d > 0]
+    deltas = np.diff(price)
+    seed = deltas[:n+1]
+    up  = seed[seed >= 0].sum()/n
+    down = -seed[seed < 0].sum()/n
+    RS = up/down
+    RSI = np.zeros_like(price)
+    RSI[:n] = 100. - 100./(1.+ RS)
 
-
-            for i in dDown
-            up = sum([])
-
-            up = (up*(n-1) + upval)/n
-            down = (down*(n-1) + downval)/n
-                    rs = up/down
-                    rsi[i] = 100. – 100./(1.+rs)
-
-        print dUp, dDown
-
-def rsi(prices, n=14):
-    
-
-
-
-
-
-#     f = open('vol' + ticker + '.csv', 'wb')
-# writer = csv.writer(f)
-# writer.writerows(vol)
+    for i in range(time_horizon, len(price)):
+        delta = deltas[i-1]
+        if delta > 0:
+            upval = delta
+            downval = 0.
+        else:
+            upval = 0.
+            downval = -delta
+        up = (up * (n-1) + upval)/n
+        down = (down * (n-1) + downval)/n
+        RS = up/down
+        RSI[i] = 100. - 100./(1.+ RS)
+    RSI = zip(date[30:],RSI[30:])
+    f = open('RSI_' + ticker + '.csv', 'wb')
+    writer = csv.writer(f)
+    writer.writerows(RSI)
 
 
-
-
+### load and transform data
 tickers = loadTickers()
+### write to csv
 loadRawData(tickers)
 for ticker in tickers:
     prices = getPrices(ticker)
     volCalc(ticker, prices, time_horizon)
-    RSI(ticker, prices, time_horizon)
+    if '^' in ticker:
+        continue
+    RSICalc(ticker, prices, time_horizon)
