360ecbccd2b77472827624f705984110a62ad0f2
Danny Carr <dpcarr45@gmail.com>
2016-04-29 20:27:20 -0500

    but honestly though

--- 13F/parse.py
+++ 13F/parse.py
@@ -16,28 +16,28 @@ def extract(spark):
 
     forms = spark.textFile('hdfs:///user/carrdp/13F/*/*') \
         .map(lambda link: parse(link, cusips).saveAsTextFile('hdfs:///user/carrdp/13F/parsed')
-        
+
     forms.cache()
-    
+
     num_investors = forms.flatMap(lambda vals: [(cusip, 1) for (cusip, amt) in vals]) \ 
         .reduceByKey(lambda x, y: x + y)
-    
+
     num_shares = forms.flatMap(lambda vals: [(cusip, amt) for (cusip, amt) in vals]) \
         .reduceByKey(lambda x, y: x + y)
-    
+
     result = num_investors.keyBy(lambda (c, n): c).join(num_shares.keyBy(lambda (c1, n1): c1)) \
         .map(lambda (cusip, ((c1, inv), (c2, shares))): (cusip, inv, shares, 1.0 * shares / inv)) \ 
         .saveAsTextFile('hdfs:///user/carrdp/13F/result')
-    
+
     #TODO:  download market cap and p/e, and eventually open/close
     #       make extensible to 1000+ forms (why isn't it)
     #           Spark streaming?
     #           foreachPartition?
     #       change from S&P to XLK stocks
-    
+
 def parse(file, cusips):
     soup = BeautifulSoup(file, "xml")
-    
+
     data = []
 
     for tag in soup.find_all('infoTable'):
