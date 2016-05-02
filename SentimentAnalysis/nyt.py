ec6e92a7c44a4eb6cc54881e0f8b9d71731fab7d
Ellis Brown <elbrown47@gmail.com>
2016-05-01 19:52:14 -0500

    nyt update

--- SentimentAnalysis/nyt.py
+++ SentimentAnalysis/nyt.py
@@ -1,5 +1,8 @@
 import json
 import requests
+from sentiment import getSentiment, countPos, countNeg
+from loadTickers import positive_words, negative_words
+
 
 # NYT API vars
 APPLICATION = "SentimentML"
@@ -20,8 +23,49 @@ r2 = requests.get(base+".json",params)
 print r2.url
 parsed = json.loads(r2.content)
 resp = parsed['response']
-for p in resp['docs'][0]:
-    print p, ":",resp['docs'][1][p]
+doc = resp['docs'][1]
+# for p in doc:
+#     print p, ":",resp['docs'][1][p]
+lead_paragraph = doc["lead_paragraph"]
+headline = doc["headline"]
+abstract = doc["abstract"]
+snippet = doc["snippet"]
+keywords = doc["keywords"]
+print
+print "lead_paragraph"
+ct = lead_paragraph
+cp = countPos(ct, positive_words)
+cn =  countNeg(ct, negative_words)
+sent = getSentiment(ct, negative_words, positive_words)
+print cp, cn, ct
+print
+print "headline"
+ct = headline
+cp = countPos(ct, positive_words)
+cn =  countNeg(ct, negative_words)
+sent = getSentiment(ct, negative_words, positive_words)
+print cp, cn, ct
+print
+print "abstract"
+ct = abstract
+cp = countPos(ct, positive_words)
+cn =  countNeg(ct, negative_words)
+sent = getSentiment(ct, negative_words, positive_words)
+print cp, cn, ct
+print
+print "snippet"
+ct = snippet
+cp = countPos(ct, positive_words)
+cn =  countNeg(ct, negative_words)
+sent = getSentiment(ct, negative_words, positive_words)
+print cp, cn, ct
+print
+print "keywords"
+ct = keywords
+cp = countPos(ct, positive_words)
+cn =  countNeg(ct, negative_words)
+sent = getSentiment(ct, negative_words, positive_words)
+print cp, cn, ct
 #
 # docs
 # facets
@@ -32,3 +76,35 @@ for p in resp['docs'][0]:
 # abstract
 # snippet
 # keywords
+
+print countPos("good happy", positive_words)
+
+ct = lead_paragraph
+
+
+
+
+
+
+ct = headline
+
+
+
+
+
+
+ct = abstract
+
+
+
+
+
+
+ct = snippet
+
+
+
+
+
+
+ct = keywords
--- SentimentAnalysis/sentiment.py
+++ SentimentAnalysis/sentiment.py
@@ -1,5 +1,5 @@
 from loadTickers import *
-from googlenews import *
+from googlenews import goog_url, fmt
 from bs4 import BeautifulSoup
 from nltk.tokenize import RegexpTokenizer
 from nltk.corpus import stopwords
@@ -7,7 +7,6 @@ from nltk.corpus import stopwords
 ## TODO: fix issue with nltk.
 ## imports do not work. throws ValueError: numpy.dtype has the wrong size, try recompiling
 
-search_terms = tickers+companies
 
 def cleanText(text):
     """
@@ -29,6 +28,7 @@ def countPos(cleantext, positive):
     """
     counts positive words in cleantext
     """
+    cleanText = cleanText(cleantext)
     pos = [word for word in cleantext if word in positive]
     return len(pos)
 
@@ -45,22 +45,14 @@ def getSentiment(cleantext, negative, positive):
     """
     counts negative and positive words in cleantext and returns a score accordingly
     """
-    positive = loadPositive()
-    negative = loadNegative()
     return (countPos(cleantext, positive) - countNeg(cleantext, negative))
 
 
-
-#print get_url(query=tickers[0],month=1,day=1,year=2015)
-URLS = [goog_url(query=term,month=1,day=1,year=2015) for term in search_terms]
-# print get_content(URLS[0])
-
-#html_doc = get_content('https://www.google.com/#hl=en&gl=us&tbm=nws&authuser=0&q=facebook')
-soup = BeautifulSoup(html_doc, 'html.parser')
-
-#print soup.prettify()
-descriptions = soup.find_all('div', class_="st")
-link_titles = soup.find_all('a')
+# soup = BeautifulSoup(html_doc, 'html.parser')
+#
+# #print soup.prettify()
+# descriptions = soup.find_all('div', class_="st")
+# link_titles = soup.find_all('a')
 
 # terms = {}
 # for line in [e.get_text() for e in descriptions+link_titles]:
@@ -72,12 +64,12 @@ link_titles = soup.find_all('a')
 # for term in terms:
 #     print terms[term], term
 
-for line in [e.get_text() for e in descriptions+link_titles]:
-    ct = cleanText(line)
-    cp = countPos(ct, positive_words)
-    cn =  countNeg(ct, negative_words)
-    sent = getSentiment(ct, negative_words, positive_words)
-    print sent, ct
+# for line in [e.get_text() for e in descriptions+link_titles]:
+#     ct = cleanText(line)
+#     cp = countPos(ct, positive_words)
+#     cn =  countNeg(ct, negative_words)
+#     sent = getSentiment(ct, negative_words, positive_words)
+#     print sent, ct
 
 # for link in soup.find_all('a'):
 #     print link.get_text()
@@ -92,21 +84,14 @@ for line in [e.get_text() for e in descriptions+link_titles]:
 
 # print search_terms
 
-URLS = []
-def date_fun():
-    f = open('urls.csv','wb')
-    writer = csv.writer(f)
-    writer.writerow(["date"]+search_terms)
-    # Date handling
-    for date in daterange(START_DATE,END_DATE):
-        month,day,year = date.month,date.day,date.year
-        #if date.weekday() < 5: # is a weekday
-        app = []
-        for term in search_terms:
-            url = goog_url(query=term,month=month,day=day,year=year)
-            # content = get_content(url)
-            # perform sentiment analysis on content
-            app.append(url)
-            #URLS.append(url)
-        writer.writerow([date]+app)
-    f.close()
+# 1/1/2000 - 2/1/20000
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+# 10
+# 11 - 12
